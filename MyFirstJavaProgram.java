4a1b8059c058458cd61ed465507d5ec04e194dd0
kenenwobodo <nwobodokenechukwu2@gmail.com>
2022-08-19 13:21:09 +0100

    ternary operator and examples

--- MyFirstJavaProgram.java
+++ MyFirstJavaProgram.java
@@ -1,7 +1,28 @@
 public class MyFirstJavaProgram {
     // print hello world
+    // local variables
+    public void pupAge() {
+        int age = 10;
+        age += 7;
+        System.out.println("My age is " + age);
+    }
 
     public static void main(String []args) {
+        // create an instance of the class
+        MyFirstJavaProgram test = new MyFirstJavaProgram();
+        // call the method pupAge
+        test.pupAge();
+
+        int a, b;
+        a = 1;
+        
+        // ternary operator
+        b = (a == 10) ? 20 : 30;
+        System.out.println("The value of b is : " + b );
+
+        b = (b== 1) ? 20 : 50;
+        System.out.println("The value of b here is >> " + b);
+
         System.out.println("Hello, World!");
     }
 }
